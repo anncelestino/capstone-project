8328097aa906ecf1e9324b7ef59da2ad4b6551ab
anncelestino <you@example.com>
2024-01-10 17:25:44 -0800

    making changes in song recommendation file

--- songrecommendations.py
+++ songrecommendations.py
@@ -58,8 +58,8 @@ def song_recommendation_vis(reco_df):
 
 def save_album_image(img_url, track_id):
     r = requests.get(img_url)
-    open('img/' + track_id + '.jpg', 'wb').write(r.content)
+    open('images/' + track_id + '.jpg', 'wb').write(r.content)
     
 
 def get_album_image(track_id):
-    return Image.open('img/' + track_id + '.jpg')
+    return Image.open('images/' + track_id + '.jpg')
